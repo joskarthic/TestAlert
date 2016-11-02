dff0740c161fdb49a18e9fb32f820f07934f1a98
Karthik <joskarthick3@gmail.com>
2016-11-02 12:35:14 +0530

    initial commit

---
+++ js/app.js
@@ -0,0 +1,37 @@
+angular.module('starter', ['ionic'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+      // Don't remove this line unless you know what you are doing. It stops the viewport
+      // from snapping when text inputs are focused. Ionic handles this internally for
+      // a much nicer keyboard experience.
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.controller("ExampleController", function($scope) {
+ 
+    $scope.confirmDialog = function() {
+        navigator.notification.confirm("Checkout this confirmation dialog", function(buttonIndex) {
+            switch(buttonIndex) {
+                case 1:
+                    alert("Decline Pressed");
+                    break;
+                case 2:
+                    alert("Accept Pressed");
+                    break;
+               
+            }
+        }, "Our Title", [ "Decline", "Accept" ]);
+    }
+ 
+});
